b4d4c2119028676ca9d8318543ca1de9f56d2cf8
sandraemad <sandraemad405@gmail.com>
2026-06-22 16:10:43 +0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+    provideTranslateService(),
+    provideTranslateHttpLoader({ prefix: 'i18n/', suffix: '.json' }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxSpinnerModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('SurveyBasketClient');
+  private readonly translate = inject(TranslateService);
+  private readonly platformId = inject(PLATFORM_ID);
+
+
+  ngOnInit() {
+    const savedLang = isPlatformBrowser(this.platformId)
+      ? (localStorage.getItem('lang') ?? 'en')
+      : 'en';
+    this.translate.use(savedLang);
+  }
 }
